264ce95ce86cf8bce84e366760b102ae1cfa1d69
fukayatsu <fukayatsu@gmail.com>
2014-02-20 23:58:38 +0900

    [from now] 2014/02/20 23:58:38

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
