2ce9962786a667ecab3e35f1030a6bc3c0dee483
fukayatsu <fukayatsu@gmail.com>
2014-02-20 15:54:36 +0900

    [from now] 2014/02/20 15:54:36

--- lib/reserved_for.rb
+++ lib/reserved_for.rb
@@ -1,5 +1,16 @@
 require "reserved_for/version"
 
 module ReservedFor
-  # Your code goes here...
+  before do
+    ReservedFor.clear_all!
+    ReservedFor.usernames  = %w(alice bob charlie david)
+    ReservedFor.white_list = %w(david)
+  end
+
+  describe '.clear_all!' do
+    it 'should clear all reserved list and white list' do
+      ReservedFor.clear_all!
+      expect(ReservedFor.usernames).to be_empty
+    end
+  end
 end
