fa4287028f0b11effa1315434ead4ce5238b141d
fukayatsu <fukayatsu@gmail.com>
2014-02-20 16:52:40 +0900

    [from now] 2014/02/20 16:52:40

--- lib/reserved_for.rb
+++ lib/reserved_for.rb
@@ -3,8 +3,11 @@ require "reserved_for/version"
 module ReservedFor
   module ModuleMethods
     def clear_all!
-      @reserved_list_map = {}
-      @white_list        = Set.new
+      @reserved_list_map = { white_list: Set.new }
+    end
+
+    def any
+      @reserved_list_map.values.inject(:+) - @reserved_list_map[:white_list]
     end
 
     def method_missing(name, *args)
--- spec/reserved_for_spec.rb
+++ spec/reserved_for_spec.rb
@@ -9,9 +9,31 @@ describe ReservedFor do
 
   describe '.clear_all!' do
     it 'should clear all reserved list and white list' do
-      expect(ReservedFor.usernames).not_to be_empty
       ReservedFor.clear_all!
       expect(ReservedFor.usernames).to be_empty
     end
   end
+
+  describe '.[definded_list]' do
+    it 'should return set of reserved word' do
+      expect(ReservedFor.usernames).to eq Set.new(%w(alice bob charlie david))
+    end
+  end
+
+  describe '.[undefined_list]' do
+    it 'should return empty set' do
+      expect(ReservedFor.undefined_list_foo).to eq Set.new
+    end
+  end
+
+  describe '.any' do
+    it 'should return all reserved word excluding white_list' do
+      ReservedFor.clear_all!
+      ReservedFor.list_a = %w(1 2 3)
+      ReservedFor.list_b = %w(a b c)
+      ReservedFor.white_list = %w(3 c)
+      expect(ReservedFor.any).to eq Set.new(%w(1 2 a b))
+    end
+  end
+
 end
