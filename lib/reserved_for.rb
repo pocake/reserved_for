c590efdd3683321dee9c96d8356fa78a4a6b8bc7
fukayatsu <fukayatsu@gmail.com>
2014-02-20 16:11:32 +0900

    [from now] 2014/02/20 16:11:32

--- lib/reserved_for.rb
+++ lib/reserved_for.rb
@@ -1,6 +1,6 @@
 require "reserved_for/version"
 
-module ReservedFor
+class ReservedFor
   def self.clear_all!
   end
 end
--- spec/reserved_for_spec.rb
+++ spec/reserved_for_spec.rb
@@ -2,14 +2,13 @@ require "spec_helper"
 
 describe ReservedFor do
   before do
-    ReservedFor.clear_all!
-    ReservedFor.usernames  = %w(alice bob charlie david)
-    ReservedFor.white_list = %w(david)
+    # ReservedFor.clear_all!
+    # ReservedFor.usernames  = %w(alice bob charlie david)
+    # ReservedFor.white_list = %w(david)
   end
 
   describe '.clear_all!' do
     it 'should clear all reserved list and white list' do
-      require 'pry'; binding.pry
       ReservedFor.clear_all!
       expect(ReservedFor.usernames).to be_empty
     end
