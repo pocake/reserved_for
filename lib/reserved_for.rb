941a6d3a45da35ccee432a509ef5ebf903f75973
fukayatsu <fukayatsu@gmail.com>
2014-02-20 16:35:14 +0900

    [from now] 2014/02/20 16:35:14

--- lib/reserved_for.rb
+++ lib/reserved_for.rb
@@ -1,6 +1,22 @@
 require "reserved_for/version"
 
 module ReservedFor
-  def self.clear_all!
+  module ModuleMethods
+    def clear_all!
+      @reserved_list_map = {}
+      @white_list        = Set.new
+    end
+
+    def method_missing(name, *args)
+      case name.to_s
+      when /(.*)=$/
+        @reserved_list_map[$1.to_sym] = Set.new(args.flatten)
+      else
+        @reserved_list_map[name.to_sym] || Set.new
+      end
+    end
   end
+
+  extend ModuleMethods
+  clear_all!
 end
