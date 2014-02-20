327909f0e5c64cf32604ef905b265771dccc07a9
fukayatsu <fukayatsu@gmail.com>
2014-02-20 16:57:18 +0900

    [from now] 2014/02/20 16:57:18

--- spec/reserved_for_spec.rb
+++ spec/reserved_for_spec.rb
@@ -3,15 +3,9 @@ require "spec_helper"
 describe ReservedFor do
   before do
     ReservedFor.clear_all!
-    ReservedFor.usernames  = %w(alice bob charlie david)
-    ReservedFor.white_list = %w(david)
-  end
-
-  describe '.clear_all!' do
-    it 'should clear all reserved list and white list' do
-      ReservedFor.clear_all!
-      expect(ReservedFor.usernames).to be_empty
-    end
+    ReservedFor.usernames    = %w(alice bob charlie david)
+    ReservedFor.group_names  = %w(root wheel)
+    ReservedFor.white_list   = %w(david)
   end
 
   describe '.[definded_list]' do
@@ -22,18 +16,21 @@ describe ReservedFor do
 
   describe '.[undefined_list]' do
     it 'should return empty set' do
-      expect(ReservedFor.undefined_list_foo).to eq Set.new
+      expect(ReservedFor.undefined_list_foo).to be_empty
     end
   end
 
   describe '.any' do
     it 'should return all reserved word excluding white_list' do
-      ReservedFor.clear_all!
-      ReservedFor.list_a = %w(1 2 3)
-      ReservedFor.list_b = %w(a b c)
-      ReservedFor.white_list = %w(3 c)
-      expect(ReservedFor.any).to eq Set.new(%w(1 2 a b))
+      expect(ReservedFor.any).to eq Set.new(%w(alice bob charlie root wheel))
     end
   end
 
+  describe '.clear_all!' do
+    it 'should clear all reserved list and white list' do
+      ReservedFor.clear_all!
+      expect(ReservedFor.any).to        be_empty
+      expect(ReservedFor.white_list).to be_empty
+    end
+  end
 end
